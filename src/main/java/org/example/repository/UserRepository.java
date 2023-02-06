49be704a9b8c061d80047c5a175d8175f6e22eac
Людок <skobeleva.ludmila@yandex.ru>
2023-02-06 14:08:16 +0300

    Первый коммит

--- src/main/java/org/example/repository/UserRepository.java
+++ src/main/java/org/example/repository/UserRepository.java
@@ -1,2 +1,17 @@
-package org.example.repository;public class UserRepository {
+package org.example.repository;
+
+import org.example.user.User;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class UserRepository {
+
+    private Map<Integer, User> allUsers = new HashMap<>();
+
+    public List<User> getAllUser(){
+        return new ArrayList<User>(allUsers.values());
+    }
 }
--- src/main/java/org/example/user/User.java
+++ src/main/java/org/example/user/User.java
@@ -1,2 +1,41 @@
-package org.example.user;public class User {
+package org.example.user;
+
+import java.util.Objects;
+
+public class User {
+    private final String login;
+    private final String password;
+
+    public User(String login, String password) {
+        this.login = login;
+        this.password = password;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof User user)) return false;
+        return Objects.equals(login, user.login) && Objects.equals(password, user.password);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(login, password);
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "login='" + login + '\'' +
+                ", password='" + password + '\'' +
+                '}';
+    }
 }

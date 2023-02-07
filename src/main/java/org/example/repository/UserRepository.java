0f115e6762bcf2d07921b93a20d79153d075541f
Людок <skobeleva.ludmila@yandex.ru>
2023-02-07 22:56:34 +0300

    Первый коммит

--- src/main/java/org/example/repository/UserRepository.java
+++ src/main/java/org/example/repository/UserRepository.java
@@ -8,25 +8,24 @@ import java.util.*;
 public class UserRepository {
 
     private List<User> users = new ArrayList<>();
+    private Object Optional;
 
     public Collection<User> getAllUser() {
         return Collections.unmodifiableCollection(users);
     }
 
     public Optional<User> getByLogin(String login) {
-        if (getAllUser()
+        getAllUser()
                 .stream()
-                .filter(user -> login.equals(user.getLogin()))){
-
-        }else {
-        return Optional<User>;
+                .filter(user -> login.equals(user.getLogin()));
+        return Optional <User>;
     }
 
     public User getByLoginAndPassword(String login, String password) {
         getAllUser().stream().anyMatch(user -> user.equals(user));
         return Optional < User >;
-
     }
+
     public void addUser(User user) {
         List.of(user);
     }
--- src/main/java/org/example/service/UserService.java
+++ src/main/java/org/example/service/UserService.java
@@ -4,6 +4,8 @@ import org.example.exception.UserNonUniqueException;
 import org.example.repository.UserRepository;
 import org.example.user.User;
 
+import java.util.List;
+
 
 public class UserService {
 
@@ -11,6 +13,12 @@ public class UserService {
 
     public UserService(UserRepository userRepository) {
         this.userRepository = userRepository;
+    }
+
+    public User loginOfUsers(){
+
+
+        return List<User>
     }
 
     public void addNewUser(String login, String passvord){
